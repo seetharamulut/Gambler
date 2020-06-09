a887d5a281047648e5c535d276f1ba6a6c21f3cb
Seetharamulu T <thumurugoti.seetharamulu@gmail.com>
2020-06-09 17:29:32 +0530

    added condition to play game for few days

--- gambler_simulation.sh
+++ gambler_simulation.sh
@@ -8,15 +8,33 @@ MIN_BET_STAKE=1
 read -p "enter the percentage of stack won or lost to quit game  " Quit_Percentage
 Quit_Stake=$(( ($Quit_Percentage*$BASE_STAKE)/100 ))
 
-Stake_Left=$BASE_STAKE
+read -p "enter num of days to play the game" Days_Limit
 
-while [ $Stake_Left -lt $(( $BASE_STAKE+$Quit_Stake )) ] && [ $Stake_Left -gt $(($BASE_STAKE-$Quit_Stake)) ]
+function GamePerDay(){
+
+	Stake_Left=$BASE_STAKE
+
+	while [ $Stake_Left -lt $(( $BASE_STAKE+$Quit_Stake )) ] && [ $Stake_Left -gt $(($BASE_STAKE-$Quit_Stake)) ]
+	do
+		case $(( $RANDOM%2 )) in
+
+			0) Stake_Left=$(( $Stake_Left-1 )) ;;
+
+			1) Stake_Left=$(( $Stake_Left+1 )) ;;
+		esac
+	done
+
+
+}
+
+for (( num_of_days=1; num_of_days<=$Days_Limit; num_of_days++ ))
 do
-	case $(( $RANDOM%2 )) in
+	GamePerDay 
 
-		0) Stake_Left=$(( $Stake_Left-1 )) ;;
-		1) Stake_Left=$(( $Stake_Left+1 )) ;;
-	esac
+	if [ $Stake_Left -gt $BASE_STAKE ]
+	then
+		echo "amount won on day "$num_of_days" is :"$(( $Stake_Left-$BASE_STAKE ))
+	else
+		 echo "amount lost on day "$num_of_days" is :"$(( $BASE_STAKE-$Stake_Left ))
+	fi
 done
-
-echo "player quit the game with stake reamaining : "$Stake_Left
